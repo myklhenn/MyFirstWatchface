cde141a09118df402da533d35034e6f296167bc4
Michael Henning <myklhenn@icloud.com>
2015-07-01 17:57:24 -0700

    Added time and date elements; it works

--- src/app.js
+++ src/app.js
@@ -1,60 +1,37 @@
-/**
- * Welcome to Pebble.js!
- *
- * This is where you write your app.
- */
 
 var UI = require('ui');
 var Vector2 = require('vector2');
 
-var main = new UI.Card({
-  title: 'Pebble.js',
-  icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
-  body: 'Press any button.'
+var main = new UI.Window({
+  fullscreen: true
 });
+
+var main_time = new UI.TimeText({
+  text: '%I:%M',
+  position: new Vector2(0, 45),
+  size: new Vector2(108, 42),
+  font: 'bitham-42-light',
+  textAlign: 'right'
+});
+
+var am_pm = new UI.TimeText({
+  text: '%p',
+  position: new Vector2(108, 63),
+  size: new Vector2(25, 24),
+  font: 'gothic-24',
+  textAlign: 'right'
+});
+
+var main_date = new UI.TimeText({
+  text: '%a %B %d',
+  position: new Vector2(0, 90),
+  size: new Vector2(144, 18),
+  font: 'gothic-18',
+  textAlign: 'center'
+});
+
+main.add(main_time);
+main.add(am_pm);
+main.add(main_date);
 
 main.show();
-
-main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }]
-    }]
-  });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
-});
-
-main.on('click', 'select', function(e) {
-  var wind = new UI.Window({
-    fullscreen: true,
-  });
-  var textfield = new UI.Text({
-    position: new Vector2(0, 65),
-    size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
-    textAlign: 'center'
-  });
-  wind.add(textfield);
-  wind.show();
-});
-
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
-});
